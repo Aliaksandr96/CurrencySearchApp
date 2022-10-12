8cdb2707ab92e6977fd4a2e04ad0d1e8fe0fffd5
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-10-12 13:52:01 +0200

    add markdowns in custom cell

--- CurrencySearchApp/CurrencyTableViewCell/CurrencyTableViewCell.swift
+++ CurrencySearchApp/CurrencyTableViewCell/CurrencyTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class CurrencyTableViewCell: UITableViewCell {
     
+    // MARK: UI Elements
     @IBOutlet weak var currencyNameLabel: UILabel!
     @IBOutlet weak var currencyPriceLabel: UILabel!
     
@@ -18,11 +19,14 @@ class CurrencyTableViewCell: UITableViewCell {
         currencyNameLabel.font = UIFont.systemFont(ofSize: 20)
         currencyPriceLabel.font = UIFont.systemFont(ofSize: 20)
     }
+    
+    // MARK: - Set Label's
     func setLabels(nameLabel: String, priceLabel: String) {
         currencyNameLabel.text = nameLabel
         currencyPriceLabel.text = priceLabel
     }
     
+    // MARK: - Settings Layer Cell
     func settingLayerCell(cell: UITableViewCell) {
           cell.layer.cornerRadius = 15
           cell.layer.borderWidth = 10
