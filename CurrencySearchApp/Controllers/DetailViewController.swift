45a2d71ce026130133bdb432d05f770bad0183dd
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-10-12 18:18:57 +0200

    add new color set in Detail VC

--- CurrencySearchApp/Controllers/DetailViewController.swift
+++ CurrencySearchApp/Controllers/DetailViewController.swift
@@ -17,7 +17,7 @@ final class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(named: "backgroundColor")
+        view.backgroundColor = UIColor(named: "detailViewBackgroundColor")
         
         setupNameCurrencyLabel()
         setupPriceCurrencyLabel()
